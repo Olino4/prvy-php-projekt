32955e77483b61b9a4d529ecc4a59766396b0e55
Olino4 <145745022+Olino4@users.noreply.github.com>
2023-10-09 20:32:18 +0200

    prvy php script

---
+++ pokus.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>konecne realny subor</h1>
+
+<?php
+echo "Ahoj";
+?>
+ <?php 
+ $color = "cierna";
+ echo "moja obvlubena farba je". $color.  "<br>";
+?>
+
+<?php
+echo time(). "<br>";
+echo date("Y-m-d H:i:s"). "<br>";
+?>
+</body>
+</html>
